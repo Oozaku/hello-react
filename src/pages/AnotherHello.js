b57da71db30b9135325a54935ae78eb9142b163d
Oozaku <willian.ozako@gmail.com>
2020-10-24 11:10:43 -0300

    add dummie page just to practice multiple pages in react

---
+++ src/pages/AnotherHello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import Header from '../components/Header'
+
+function AnotherHello () {
+  return (
+    <div>
+      <Header title="I'm another page!" />
+    </div>
+  );
+}
+
+export default AnotherHello;
