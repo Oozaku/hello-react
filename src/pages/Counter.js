cb71eb4b389f04efecf9642ad3fbee6d5174e5f1
Oozaku <willian.ozako@gmail.com>
2020-10-28 18:25:19 -0300

    use conditional rendering in counter

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -3,6 +3,7 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid'
 import TopPanel from '../components/TopPanel'
 import { Typography } from '@material-ui/core';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
@@ -15,18 +16,29 @@ const Counter = () => {
 
         <Grid item xs={12}>
           <Typography variant='h3' align='center'>Counter 0 to 5!</Typography>
-          <Typography variant='h3' align='center' color='primary'>{count}</Typography>
+          <br/> 
+          <Typography variant='h3' align='center' color='primary'>Count = {count}</Typography>
         </Grid>
 
         <Grid container item xs={12} justify='center'>
           <Button
             color='primary'
-            variant='outlined'
+            variant='contained'
             onClick={() => setCount((count + 1) % 6)}>
-            Increment it!
+            <AddCircleIcon style={{ fontSize:  48}}/>
           </Button>
         </Grid>
-      
+
+        <Grid container item xs={12} justify='center'> {/*check if count is a odd or even number*/}
+          {
+            count % 2 == 0? (
+              <Typography variant='h3'>Count is a even number!</Typography>
+            ):(
+              <Typography variant='h3'>Count is a odd number!</Typography>
+            )
+          }
+        </Grid>
+
       </Grid>
     </div>
     
